263c41dd4799dd80d1346f9704216838039c03d6
AntonAGAA <127387653+AntonAGAA@users.noreply.github.com>
2023-10-05 09:32:28 +0000

    started (25 mins to understand task)

---
+++ Assignment02/solution.py
@@ -0,0 +1,8 @@
+# opgaven består i at søge efter strings i den speciferede streng og returnere start index for hvor ordet er.
+# det første tal angiver hvor mange gange der skal søges i den n+1 linje
+
+
+
+
+
+def solution():
